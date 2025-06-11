01a1100987f29862f31b8c4a21a573cf5658b288
LeoVM3007 <leiv3007@yandex.ru>
2025-06-11 18:53:06 +0300

    Исправлены ошибки

--- src/main/java/ru/yandex/practicum/filmorate/exception/FilmExtractionException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exception;
-
-public class FilmExtractionException extends RuntimeException {
-    public FilmExtractionException(String message) {
-        super(message);
-    }
-}
